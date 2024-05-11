75199aee136dcf8ae3590c99e82b8250ae598eed
David V. Smith <david.v.smith@temple.edu>
2024-05-11 09:54:07 -0400

    copy from rf1-mbme-pilot

---
+++ code/run_warpkit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "scriptdir: ${scriptdir}"
+
+rm -rf $scriptdir/missingFiles-warpkit.log
+touch $scriptdir/missingFiles-warpkit.log
+
+sub=$1
+
+#for sub in `cat ${scriptdir}/sublist-deriv.txt` ; do
+	for acq in mb1me4 mb2me4 mb3me3 mb3me3ip0 mb3me4 mb3me4fa50 mb6me4 ; do
+		script=${scriptdir}/warpkit.sh
+		NCORES=5
+		while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
+			sleep 5s
+		done
+	   bash $script $sub $acq &
+	done 
+#done
