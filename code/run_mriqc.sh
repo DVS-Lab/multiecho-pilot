32db06da50ba06d22f64803bf173ae8b09ba7db0
jameswyngaarden <james.wyngaarden@temple.edu>
2024-05-20 15:24:12 -0400

    update subs

--- code/run_mriqc.sh
+++ code/run_mriqc.sh
@@ -4,8 +4,8 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-#for sub in `cat ${scriptdir}/sublist_all.txt` ; do
-for sub in 10606sp 10391 10003 ; do
+for sub in `cat ${scriptdir}/sublist_all.txt` ; do
+#for sub in 10606sp 10391 10003 ; do
 	script=${scriptdir}/mriqc.sh
 	NCORES=5
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
