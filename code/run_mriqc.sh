c62efe325c9fa146ca59d3d1f582333314dcc43d
jameswyngaarden <james.wyngaarden@temple.edu>
2024-05-20 15:25:19 -0400

    fix sublist-all.txt

--- code/run_mriqc.sh
+++ code/run_mriqc.sh
@@ -4,7 +4,7 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-for sub in `cat ${scriptdir}/sublist_all.txt` ; do
+for sub in `cat ${scriptdir}/sublist-all.txt` ; do
 #for sub in 10606sp 10391 10003 ; do
 	script=${scriptdir}/mriqc.sh
 	NCORES=5
