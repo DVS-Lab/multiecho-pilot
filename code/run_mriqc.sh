e3aa64cb7d9aaa7a68add9ce353559b04dd40acf
jameswyngaarden <james.wyngaarden@temple.edu>
2024-05-23 15:33:08 -0400

    test intended for update script

--- code/run_mriqc.sh
+++ code/run_mriqc.sh
@@ -4,10 +4,10 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-for sub in `cat ${scriptdir}/sublist-all.txt` ; do
-#for sub in 10606sp 10391 10003 ; do
+for sub in 10017; do
+#for sub in `cat ${scriptdir}/sublist-int.txt` ; do
 	script=${scriptdir}/mriqc.sh
-	NCORES=5
+	NCORES=10
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
 		sleep 1s
 	done
