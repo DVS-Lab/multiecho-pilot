e0e306eaced7b57cea534aea90eb8f1e3ba0a5c2
jameswyngaarden <james.wyngaarden@temple.edu>
2024-07-12 12:32:37 -0400

    run prepdata on sp subs

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -4,7 +4,7 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-for sub in `cat ${scriptdir}/sublist-all.txt` ; do
+for sub in `cat ${scriptdir}/sublist-sp.txt` ; do
 
 	# one at a time to avoid race conditions
 	bash ${scriptdir}/prepdata.sh $sub
