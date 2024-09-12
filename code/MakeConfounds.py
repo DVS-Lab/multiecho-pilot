b4eeff2380ff31c859395ccb00a14434bacbe722
jameswyngaarden <james.wyngaarden@temple.edu>
2024-09-11 21:15:18 -0400

    fix confounds

--- code/MakeConfounds.py
+++ code/MakeConfounds.py
@@ -1,6 +1,6 @@
 # Extracts confound regressors for use in feat
 # E.G. use
-#$ python MakeConfounds.py --fmriprepDir="/data/projects/Tensor_game/Data/Raw/NARPS/derivatives/fmriprep"
+#$ python MakeConfounds.py --fmriprepDir="/ZPOOL/data/projects/Tensor_game/Data/Raw/NARPS/derivatives/fmriprep"
 #
 # originally written by Jeff Dennison
 # latest version here: https://github.com/DVS-Lab/general/blob/master/MakeConfounds.py
@@ -32,9 +32,9 @@ for f in cons:
 
 	# parse key-value pairs and build variables
 	sub=re.search('/func/(.*)_task', f).group(1)    
-	#acq=re.search('_acq-(.*)_', f).group(1)
-	run=re.search('_run-(.*)_desc',f).group(1)
-	task=re.search('_task-(.*)_run',f).group(1)
+	acq=re.search('_acq-(.*)_', f).group(1)
+	#run=re.search('_run-(.*)_desc',f).group(1)
+	task=re.search('_task-(.*)_',f).group(1)
 	derivatives_path=re.search('(.*)fmriprep/sub',f).group(1)
 
 	#read in confound file and build matrix
@@ -54,10 +54,12 @@ for f in cons:
 	df_all.fillna(0, inplace=True)
 
 	# generate output files
-	outfile="%s_task-%s_run-%s_desc-fslConfounds.tsv"%(sub,task,run)
+	#outfile="%s_task-%s_run-%s_desc-fslConfounds.tsv"%(sub,task,run)
+	outfile="%s_task-%s_acq-%s_desc-fslConfounds.tsv"%(sub,task,acq)	
 	outdir=derivatives_path+"fsl/confounds/%s/" %(sub)
 	if not os.path.exists(outdir):
 		os.makedirs(outdir)
 	output=outdir+outfile
-	print(sub,run,task)
+	print(sub,acq,task)	
+	#print(sub,run,task)
 	df_all.to_csv(output,index=False,sep='\t',header=False)
