66a702f162cd67c2a27053a94068914575fc952f
cooper-sharp <tun31934@temple.edu>
2024-12-18 20:51:11 -0500

    Update run script

--- code/run_L1stats.sh
+++ code/run_L1stats.sh
@@ -6,26 +6,23 @@ basedir="$(dirname "$scriptdir")"
 
 task=sharedreward # edit if necessary
 
-for denoise in "base" "tedana";do # "base" = aCompCor confounds; "tedana" = aCompCor + tedana
-	for ppi in "0" "VS_thr5"; do #"VS_thr5"; do #"VS_thr5"; do # putting 0 first will indicate "activation" "VS_thr5"
+for denoise in "base"; do # "base" = aCompCor confounds; "tedana" = aCompCor + tedana
+	for ppi in "VS_thr5"; do #"VS_thr5"; do #"VS_thr5"; do # putting 0 first will indicate "activation" "VS_thr5"
 		for model in 1; do
 
-			#for sub in 10137 10150 10223 10234 10422 10438; do
-			#for sub in 10137 10150 10221 10223 10234 10296 10438; do
-			#for sub in 10094 10137 10150 10221 10223 10234 10296 10438; do
 			#for sub in 10137; do
-			#for sub in `cat ${scriptdir}/sublist-complete.txt`; do # `ls -d ${basedir}/derivatives/fmriprep/sub-*/`
-			for sub in 10589sp 10590sp 10603sp 10606sp 10608sp 10640sp 10644sp 10690sp 10691sp 10723sp 10741sp 10777sp 10803sp; do
-
-			  sub=${sub#*sub-}
-			  sub=${sub%/}  
-
-			  for mbme in mb3me1fa50; do
-			  #for mbme in mb1me1 mb1me4 mb3me1 mb3me4 mb6me1 mb6me4 mb2me4 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4fa50; do
-			  #for mbme in mb1me1 mb1me4 mb3me1 mb3me4 mb6me1 mb6me4; do
-			  #for mbme in mb1me4; do
-			  # for mb in 1 3 6; do
-				# for me in 1 4; do
+			for sub in `cat ${scriptdir}/sublist-complete.txt`; do # `ls -d ${basedir}/derivatives/fmriprep/sub-*/`
+				sub=${sub#*sub-}
+				sub=${sub%/}
+			
+			if [[ $sub == *sp ]]; then
+				acqs=("mb2me4" "mb3me1fa50" "mb3me3" "mb3me3fa50" "mb3me4" "mb3me4fa50")
+			else
+				acqs=("mb1me1" "mb1me4" "mb3me1" "mb3me4" "mb6me1" "mb6me4")
+			fi
+			
+			#for mbme in mb1me1 mb1me4 mb3me1 mb3me4 mb6me1 mb6me4 mb2me4 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4fa50; do
+			for mbme in "${acqs[@]}"; do
 
 			  	# Manages the number of jobs and cores
 			  	SCRIPTNAME=${basedir}/code/L1stats.sh
