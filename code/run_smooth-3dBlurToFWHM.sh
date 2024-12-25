68450136f8f94051e6743cf7763c6b2bb49a9d81
David V. Smith <david.v.smith@temple.edu>
2024-12-25 10:25:16 -0500

    initial commit

---
+++ code/run_smooth-3dBlurToFWHM.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+basedir="$(dirname "$scriptdir")"
+
+task=sharedreward # edit if necessary
+
+for sub in `cat ${scriptdir}/sublist-complete.txt`; do # `ls -d ${basedir}/derivatives/fmriprep/sub-*/`
+	sub=${sub#*sub-}
+	sub=${sub%/}
+
+	if [[ $sub == *sp ]]; then
+		acqs=("mb2me4" "mb3me1fa50" "mb3me3" "mb3me3fa50" "mb3me4" "mb3me4fa50")
+	else
+		acqs=("mb1me1" "mb1me4" "mb3me1" "mb3me4" "mb6me1" "mb6me4")
+	fi
+	
+	for mbme in "${acqs[@]}"; do
+	
+	  	# Manages the number of jobs and cores
+	  	SCRIPTNAME=${basedir}/code/run_smooth-3dBlurToFWHM.sh
+	  	NCORES=15
+	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+	    		sleep 5s
+	  	done
+	  	bash $SCRIPTNAME $sub $mbme &
+		echo $SCRIPTNAME $sub $mbme &
+		sleep 1s
+	done
+done
---
+++ code/smooth-3dBlurToFWHM.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+istartdatadir=/ZPOOL/data/projects/multiecho-pilot #need to fix this upon release (no hard coding paths)
+
+# study-specific inputs
+TASK=sharedreward
+sm=4
+sub=$1
+mbme=$2
+acq=${mbme}
+
+
+if [ "$mbme" == "mb1me1" -o  "$mbme" == "mb3me1" -o "$mbme" == "mb6me1" -o "$mbme" == "mb3me1fa50" ]; then
+	INDATA=${istartdatadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_acq-${acq}_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz
+	OUTDATA=${istartdatadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_acq-${acq}_space-MNI152NLin6Asym_desc-preproc_bold_${sm}mm.nii.gz
+	MASK=${istartdatadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_acq-${acq}_space-MNI152NLin6Asym_desc-brain_mask.nii.gz
+else
+	INDATA=${istartdatadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_acq-${acq}_part-mag_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz
+	OUTDATA=${istartdatadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_acq-${acq}_part-mag_space-MNI152NLin6Asym_desc-preproc_bold_${sm}mm.nii.gz
+	MASK=${istartdatadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_acq-${acq}_part-mag_space-MNI152NLin6Asym_desc-brain_mask.nii.gz
+fi
+
+if [ ! -e $INDATA ]; then
+	echo ${sub} ${acq} "No data"
+	exit
+fi
+
+3dBlurToFWHM -FWHM $sm -input $INDATA -prefix $OUTDATA -mask $MASK
+
