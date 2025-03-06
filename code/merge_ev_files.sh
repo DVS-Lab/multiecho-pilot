2325fc0727662bdfd2ee1ad362ff3a80a49dc453
jameswyngaarden <james.wyngaarden@temple.edu>
2025-03-06 18:16:04 -0500

    fix merged EVs for face vs computer

--- code/merge_ev_files.sh
+++ code/merge_ev_files.sh
@@ -34,15 +34,13 @@ for sub in `cat ${scriptdir}/sublist-all.txt`; do
                     sort -k1,1n > "$sub_dir/_guess_allRightButton.txt"
 
                 # Merge _outcome_computer_reward.txt, _outcome_computer_punish.txt, and _outcome_computer_neutral.txt
-                cat "$sub_dir/_outcome_computer_reward.txt" \
-                    "$sub_dir/_outcome_computer_punish.txt" \
-                    "$sub_dir/_outcome_computer_neutral.txt" | \
+                cat "$sub_dir/_guess_leftButton_computer.txt" \
+                    "$sub_dir/_guess_rightButton_computer.txt" | \
                     sort -k1,1n > "$sub_dir/_guess_allComputer.txt"
 
                 # Merge _outcome_stranger_reward.txt, _outcome_stranger_punish.txt, and _outcome_stranger_neutral.txt
-                cat "$sub_dir/_outcome_stranger_reward.txt" \
-                    "$sub_dir/_outcome_stranger_punish.txt" \
-                    "$sub_dir/_outcome_stranger_neutral.txt" | \
+                cat "$sub_dir/_guess_leftButton_face.txt" \
+                    "$sub_dir/_guess_rightButton_face.txt" | \
                     sort -k1,1n > "$sub_dir/_guess_allFace.txt"
 
                 echo "    Finished processing subdirectory: $sub_dir"
