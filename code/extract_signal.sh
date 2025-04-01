44066fd1c606463969f775321ce8a3f043ec2629
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2025-04-01 10:17:51 -0400

    Update extract_signal.sh

--- code/extract_signal.sh
+++ code/extract_signal.sh
@@ -4,12 +4,11 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 basedir="$(dirname "$scriptdir")"
 
-input=tsnr # tsnr, beta, or zstat
+input=beta # tsnr, beta, or zstat
 
 for denoise in "base"; do # "base" "tedana" 
-  for mask in "VSconstrained" "VMPFC" "rightMotor" "leftMotor" "rightCerebellum" "leftCerebellum" "rFFA"; do
-  #for mask in "leftMotor" "rightMotor" "leftCerebellum" "rightCerebellum"; do
-  #for mask in "leftMotor"; do
+  for mask in "ME_bonf" "HCxME_bonf" "3way_bonf"; do
+  #for mask in "VSconstrained" "VMPFC" "rightMotor" "leftMotor" "rightCerebellum" "leftCerebellum" "rFFA"; do
     for ppi in "act"; do # "act" "ppi_seed-VS"
       for sub in $(cat ${scriptdir}/sublist-included.txt); do 
         sub=${sub#*sub-}
@@ -43,6 +42,8 @@ for denoise in "base"; do # "base" "tedana"
             tsnr_image="/ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/sub-${sub}/L1_task-sharedreward_model-1_type-act_acq-${mbme}_sm-5_denoising-base_forTSNR.feat/tsnr.nii.gz"
 	    beta_image="/ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/sub-${sub}/L1_task-sharedreward_model-3_type-${ppi}_acq-${mbme}_sm-0_denoising-${denoise}.feat/stats/cope3.nii.gz"
             zstat_image="/ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/sub-${sub}/L1_task-sharedreward_model-3_type-${ppi}_acq-${mbme}_sm-0_denoising-${denoise}.feat/stats/zstat3.nii.gz"
+          elif [[ "$mask" == "ME_bonf" ]] || [[ "$mask" == "HCxME_bonf" ]] || [[ "$mask" == "3way_bonf" ]]; then
+	    beta_image="/ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/sub-${sub}/L1_task-sharedreward_model-1_type-${ppi}_acq-${mbme}_sm-0_denoising-${denoise}.feat/stats/cope13.nii.gz"
           else
             echo "Invalid mask: $mask"
             continue
