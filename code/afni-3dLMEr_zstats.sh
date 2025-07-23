2c8fb78f1b9d761d62c2adffb7b7ddd8ec8d5d30
David V. Smith <david.v.smith@temple.edu>
2025-07-23 15:21:37 -0400

    changes for running zstats

--- code/afni-3dLMEr_zstats.sh
+++ code/afni-3dLMEr_zstats.sh
@@ -6,10 +6,10 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
 
-## Estimate models
+### -- Estimate models
 
 # ppi phys regressor
-3dLMEr -prefix ${maindir}/derivatives/fsl/LME_output_ppi-zstat17 \
+3dLMEr -prefix /ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/LME_output_ppi-zstat17 \
       -jobs 8 \
       -model "HC+MB+ME+HC:MB+HC:ME+MB:ME+HC:MB:ME+(1|Subj)" \
       -gltCode MB3_vs_MB1 "MB : +1*MB3 -1*MB1" \
@@ -41,179 +41,195 @@ maindir="$(dirname "$scriptdir")"
       -dataTable @LME_table_corrected_act-zstat13.txt
 
 
-# tsnr
-3dLMEr -prefix /ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/LME_output_tsnr \
-      -jobs 8 \
-      -model "HC+MB+ME+HC:MB+HC:ME+MB:ME+HC:MB:ME+(1|Subj)" \
-      -gltCode MB3_vs_MB1 "MB : +1*MB3 -1*MB1" \
-      -gltCode MB6_vs_MB1 "MB : +1*MB6 -1*MB1" \
-      -gltCode ME4_vs_ME1 "ME : +1*ME4 -1*ME1" \
-      -gltCode HC_vs_20ch "HC : +1*64ch -1*20ch" \
-      -dataTable @LME_table_corrected_tsnr.txt
 
+### -- PPI cope17 effects (phys regressor)
 
+# MB3 vs MB1
+3dFWHMx -mask brain_mask.nii.gz -acf \
+  -input /ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/LME_output_ppi-zstat17+tlrc'[8]' \
+  > acf_MB3_vs_MB1_ppi_zstat17.txt
 
-## PPI phys post-hoc tests
+# MB6 vs MB1
+3dFWHMx -mask brain_mask.nii.gz -acf \
+  -input /ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/LME_output_ppi-zstat17+tlrc'[10]' \
+  > acf_MB6_vs_MB1_ppi_zstat17.txt
 
-3dFWHMx -input LME_output_ppi-zstat17+tlrc'[6]'         -mask brain_mask.nii.gz         -acf > acf_3way.txt
-3dFWHMx -input LME_output_ppi-zstat17+tlrc'[8]' -mask brain_mask.nii.gz -acf > acf_MB3_vs_MB1.txt
-3dFWHMx -input LME_output_ppi-zstat17+tlrc'[12]' -mask brain_mask.nii.gz -acf > acf_ME4_vs_ME1.txt
-3dFWHMx -input LME_output_ppi-zstat17+tlrc'[1]' -mask brain_mask.nii.gz -acf > acf_MB_main.txt
-3dFWHMx -input LME_output_ppi-zstat17+tlrc'[2]' -mask brain_mask.nii.gz -acf > acf_ME_main.txt
-3dFWHMx -input LME_output_ppi-zstat17+tlrc'[6]' -mask brain_mask.nii.gz -acf > acf_3way.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_ppi-zstat13+tlrc'[8]' > acf_MB3_vs_MB1_zstat13.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_ppi-zstat13+tlrc'[10]' > acf_MB6_vs_MB1_zstat13.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_ppi-zstat13+tlrc'[12]' > acf_ME4_vs_ME1_zstat13.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_ppi-zstat13+tlrc'[14]' > acf_HC_vs_20ch_zstat13.txt
+# ME4 vs ME1
+3dFWHMx -mask brain_mask.nii.gz -acf \
+  -input /ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/LME_output_ppi-zstat17+tlrc'[12]' \
+  > acf_ME4_vs_ME1_ppi_zstat17.txt
+
+# HC vs 20ch
+3dFWHMx -mask brain_mask.nii.gz -acf \
+  -input /ZPOOL/data/projects/multiecho-pilot/derivatives/fsl/LME_output_ppi-zstat17+tlrc'[14]' \
+  > acf_HC_vs_20ch_ppi_zstat17.txt
+
 
 
 3dClustSim -mask brain_mask.nii.gz \
-           -acf 0.879067 2.13714 13.4847 \
-           -prefix ClustSim_safest
+           -acf 0.892078 2.12894 14.2735 \
+           -prefix ClustSim_ppi_zstat17_bonf \
+           -pthr 0.05 -athr 0.00714
 
+
+
+
+# HC main effect
 3dClusterize -inset LME_output_ppi-zstat17+tlrc \
-             -ithr 6 -NN 1 -clust_nvox 60 -bisided p=0.05 \
-             -pref_map LME_output_FWER_3way_FINAL
+             -ithr 0 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map HC_main_ppi_zstat17_thresh
+#nothing
+
+# MB main effect
+3dClusterize -inset LME_output_ppi-zstat17+tlrc \
+             -ithr 1 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map MB_main_ppi_zstat17_thresh
+
+# ME main effect
+3dClusterize -inset LME_output_ppi-zstat17+tlrc \
+             -ithr 2 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map ME_main_ppi_zstat17_thresh
 
 
 # 2-way interactions
 3dClusterize -inset LME_output_ppi-zstat17+tlrc \
-             -ithr 3 -NN 1 -clust_nvox 60 -bisided p=0.05 \
-             -pref_map LME_output_FWER_HCxMB
+             -ithr 3 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map HCxMB_ppi_zstat17_thresh
+# nothing here
+
+3dClusterize -inset LME_output_ppi-zstat17+tlrc \
+             -ithr 4 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map HCxME_ppi_zstat17_thresh
+# nothing here
+
+3dClusterize -inset LME_output_ppi-zstat17+tlrc \
+             -ithr 5 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map MBxME_ppi_zstat17_thresh
+
+# 3-way interactions
+3dClusterize -inset LME_output_ppi-zstat17+tlrc \
+             -ithr 6 -NN 1 -clust_nvox 78 -bisided p=0.05 \
+             -pref_map 3way_ppi_zstat17_thresh
 # nothing here
 
 
-3dClusterize -inset LME_output_ppi-zstat17+tlrc \
-             -ithr 4 -NN 1 -clust_nvox 60 -bisided p=0.05 \
-             -pref_map LME_output_FWER_HCxME
+3dAFNItoNIFTI -prefix MB_main_ppi_zstat17_thresh.nii.gz MB_main_ppi_zstat17_thresh+tlrc
+3dAFNItoNIFTI -prefix ME_main_ppi_zstat17_thresh.nii.gz ME_main_ppi_zstat17_thresh+tlrc
+3dAFNItoNIFTI -prefix MBxME_ppi_zstat17_thresh.nii.gz MBxME_ppi_zstat17_thresh+tlrc
 
-3dClusterize -inset LME_output_ppi-zstat17+tlrc \
-             -ithr 5 -NN 1 -clust_nvox 60 -bisided p=0.05 \
-             -pref_map LME_output_FWER_MBxME
 
-3dAFNItoNIFTI -prefix LME_output_FWER_HCxMB.nii.gz LME_output_FWER_HCxMB+tlrc # skip
-3dAFNItoNIFTI -prefix LME_output_FWER_HCxME.nii.gz LME_output_FWER_HCxME+tlrc
-3dAFNItoNIFTI -prefix LME_output_FWER_MBxME.nii.gz LME_output_FWER_MBxME+tlrc
+mricron L3_model-1_task-sharedreward_n40_mixedeffects-flame1+2_denoising-base/L3_task-sharedreward_type-act_cnum-01_cname-C_left_onegroup_denoising-base.gfeat/mean_func.nii.gz &
+
+# striatum in all three, so this could be something to plot
 
 
 
+### -- PPI cope13 effects (reward > punish ppi)
+# summary: nothing here
 
-# take care of multiple comparisons
-
-
-3dClustSim -mask brain_mask.nii.gz \
-           -acf 0.879067 2.13714 13.4847 \
-           -prefix ClustSim_bonf \
-           -pthr 0.05 \
-           -athr 0.00714
-
-cat ClustSim_bonf.NN1_bisided.1D
-
-3dClusterize -inset LME_output_ppi-zstat17+tlrc \
-             -ithr 5 -NN 1 -clust_nvox 88 -bisided p=0.05 \
-             -pref_map LME_output_FWER_MBxME_bonf
+3dFWHMx -input LME_output_ppi-zstat13+tlrc'[8]'  -mask brain_mask.nii.gz -acf > acf_MB3_vs_MB1_ppi_zstat13.txt
+3dFWHMx -input LME_output_ppi-zstat13+tlrc'[10]' -mask brain_mask.nii.gz -acf > acf_MB6_vs_MB1_ppi_zstat13.txt
+3dFWHMx -input LME_output_ppi-zstat13+tlrc'[12]' -mask brain_mask.nii.gz -acf > acf_ME4_vs_ME1_ppi_zstat13.txt
+3dFWHMx -input LME_output_ppi-zstat13+tlrc'[14]' -mask brain_mask.nii.gz -acf > acf_HC_vs_20ch_ppi_zstat13.txt
 
 
-for fname in HC MB ME HCxMB HCxME MBxME 3way; do
-  3dAFNItoNIFTI -prefix LME_output_FWER_${fname}_bonf.nii.gz LME_output_FWER_${fname}_bonf+tlrc
+
+for effect in MB3_vs_MB1 MB6_vs_MB1 ME4_vs_ME1 HC_vs_20ch; do
+  file="acf_${effect}_ppi_zstat13.txt"
+  if [[ -f "$file" ]]; then
+    echo "Effect: $effect"
+    awk 'NR==2 { printf "ACF params: %.6f  %.6f  %.6f\n\n", $1, $2, $3 }' "$file"
+  else
+    echo "Missing: $file"
+  fi
 done
 
 
-
 3dClustSim -mask brain_mask.nii.gz \
-           -acf 0.925349 1.92623 14.7885 \
-           -prefix ClustSim_zstat13_ppi \
+           -acf 0.960875 2.092270 18.485800 \
+           -prefix ClustSim_ppi_zstat13_bonf \
            -pthr 0.05 -athr 0.00714
 
-
-# no effects for cope-13_PPI
-
-
-3dinfo -verb LME_output_act-zstat13+tlrc.HEAD
-
-
-
-
-
-# Cope 13 for activation
-# MB3 vs MB1 Z-stat = sub-brick [8]
-3dFWHMx -input LME_output_act-zstat13+tlrc'[8]' -mask brain_mask.nii.gz -acf > acf_MB3_vs_MB1_act_zstat13.txt
-
-# MB6 vs MB1 Z-stat = sub-brick [10]
-3dFWHMx -input LME_output_act-zstat13+tlrc'[10]' -mask brain_mask.nii.gz -acf > acf_MB6_vs_MB1_act_zstat13.txt
-
-# ME4 vs ME1 Z-stat = sub-brick [12]
-3dFWHMx -input LME_output_act-zstat13+tlrc'[12]' -mask brain_mask.nii.gz -acf > acf_ME4_vs_ME1_act_zstat13.txt
-
-# HC vs 20ch Z-stat = sub-brick [14]
-3dFWHMx -input LME_output_act-zstat13+tlrc'[14]' -mask brain_mask.nii.gz -acf > acf_HC_vs_20ch_act_zstat13.txt
-
-3dClustSim -mask brain_mask.nii.gz \
-           -acf 3.70353 3.83141 3.86924 \
-           -prefix ClustSim_act_zstat13 \
-           -pthr 0.05 -athr 0.00714
-
-
-# main effects and interactions
-# HC main effect
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 0 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map HC_main_act13_thresh
 
 # MB main effect
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 1 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map MB_main_act13_thresh
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 1 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map MB_main_ppi_zstat13_thresh
 
 # ME main effect
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 2 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map ME_main_act13_thresh
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 2 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map ME_main_ppi_zstat13_thresh
 
-# HC × MB interaction
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 3 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map HCxMB_act13_thresh
-
-# HC × ME interaction
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 4 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map HCxME_act13_thresh
+# HC main effect
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 0 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map HC_main_ppi_zstat13_thresh
 
 # MB × ME interaction
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 5 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map MBxME_act13_thresh
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 5 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map MBxME_ppi_zstat13_thresh
 
-# HC × MB × ME interaction
-3dClusterize -inset LME_output_act-zstat13+tlrc \
-             -ithr 6 -NN 1 -clust_nvox 159 -bisided p=0.05 \
-             -pref_map HCxMBxME_act13_thresh
+# HC × MB interaction
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 3 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map HCxMB_ppi_zstat13_thresh
+
+# HC × ME interaction
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 4 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map HCxME_ppi_zstat13_thresh
+
+# 3-way interaction
+3dClusterize -inset LME_output_ppi-zstat13+tlrc \
+             -ithr 6 -NN 1 -clust_nvox 40 -bisided p=0.05 \
+             -pref_map 3way_ppi_zstat13_thresh
+
+# NOTHING WITH COPE13 on PPI
 
 
-# no significnat whole-brain results for activation (cope 13) or PPI (cope 13)
+
+### -- activation cope13 effects (reward > punish )
 
 
+3dFWHMx -input LME_output_act-zstat13+tlrc'[8]' -mask brain_mask.nii.gz -acf > acf_MB3_vs_MB1_act_zstat13.txt
+3dFWHMx -input LME_output_act-zstat13+tlrc'[10]' -mask brain_mask.nii.gz -acf > acf_MB6_vs_MB1_act_zstat13.txt
+3dFWHMx -input LME_output_act-zstat13+tlrc'[12]' -mask brain_mask.nii.gz -acf > acf_ME4_vs_ME1_act_zstat13.txt
+3dFWHMx -input LME_output_act-zstat13+tlrc'[14]' -mask brain_mask.nii.gz -acf > acf_HC_vs_20ch_act_zstat13.txt
 
-# TSNR model follow ups
-3dinfo -verb LME_output_tsnr+tlrc.HEAD
 
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_tsnr+tlrc'[8]'  > acf_MB3_vs_MB1_tsnr.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_tsnr+tlrc'[10]' > acf_MB6_vs_MB1_tsnr.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_tsnr+tlrc'[12]' > acf_ME4_vs_ME1_tsnr.txt
-3dFWHMx -mask brain_mask.nii.gz -acf -input LME_output_tsnr+tlrc'[14]' > acf_HC_vs_20ch_tsnr.txt
+# get smoothness
+for effect in MB3_vs_MB1 MB6_vs_MB1 ME4_vs_ME1 HC_vs_20ch; do
+  file="acf_${effect}_act_zstat13.txt"
+  if [[ -f "$file" ]]; then
+    echo "Effect: $effect"
+    awk 'NR==2 { printf "ACF params: %.6f  %.6f  %.6f\n\n", $1, $2, $3 }' "$file"
+  else
+    echo "Missing: $file"
+  fi
+done
+
 
 3dClustSim -mask brain_mask.nii.gz \
-           -acf 0.175478 4.74007 26.6659 \
-           -prefix ClustSim_tsnr \
+           -acf 0.821032 2.107260 18.687500 \
+           -prefix ClustSim_act_zstat13_bonf \
            -pthr 0.05 -athr 0.00714
 
 
-3dClusterize -inset LME_output_tsnr+tlrc \
-             -ithr 8 -NN 1 -clust_nvox 5016 -bisided p=0.05 \
-             -pref_map LME_output_tsnr_MB3_vs_MB1_FWER
+# Bonferroni-corrected cluster threshold = 191 voxels
+THRESH=0.05
+CLUST=191
+INSET=LME_output_act-zstat13+tlrc
 
-3dClusterize -inset LME_output_tsnr+tlrc \
-             -ithr 8 -NN 1 -clust_nvox 5016 -bisided p=0.05 \
-             -pref_map LME_output_tsnr_MB3_vs_MB1_FWER.nii.gz
+3dClusterize -inset $INSET -ithr 0 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map HC_main_act13_thresh
+3dClusterize -inset $INSET -ithr 1 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map MB_main_act13_thresh
+3dClusterize -inset $INSET -ithr 2 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map ME_main_act13_thresh
+3dClusterize -inset $INSET -ithr 3 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map HCxMB_act13_thresh
+3dClusterize -inset $INSET -ithr 4 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map HCxME_act13_thresh
+3dClusterize -inset $INSET -ithr 5 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map MBxME_act13_thresh
+3dClusterize -inset $INSET -ithr 6 -NN 1 -clust_nvox $CLUST -bisided p=$THRESH -pref_map HCxMBxME_act13_thresh
+
+
+
 
